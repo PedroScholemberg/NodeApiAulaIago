8036e4abd1bfb94fa80a2bc07f28bea9578c2189
pgpg1812 <pedrogabrielsc1812@gmail.com>
2024-11-20 21:53:25 -0300

    ai ai ai i'm a little butterfly

---
+++ api.js
@@ -0,0 +1,36 @@
+// importa as dependencias
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = 3000;
+
+app.get('/pessoa', async (req, res) => {
+    try {
+        res.status(200).json({ message: "mensagem" });
+    } catch (error) {
+        res.status(500).json({ error: "erro" });
+    }
+});
+
+
+app.get('/pessoa/id/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        res.status(200).json({ message: `Pessoa com ID: ${id}` });
+    } catch (error) {
+        res.status(500).json({ error: "Erro ao buscar a pessoa" });
+    }
+});
+
+app.get('/pessoa/nomeouid/:id:nome', async(req, res) => {
+ const {id, nome} = req.params;
+ try{
+
+ }catch{
+    res.status()
+ }
+});
+
+app.listen(PORT, () =>{
+    console.log(`porta: ${PORT}`)
+});
